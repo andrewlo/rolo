d0bdfc7a272b0b43bad905676f874a95454868f3
Andrew Lo <andrew.lo@shopify.com>
2017-10-07 13:26:14 -0400

    Add Guest model & controller with CRUD operations

---
+++ app/controllers/guests_controller.rb
@@ -0,0 +1,49 @@
+class GuestsController < ApplicationController
+  def index
+    @guests = Guest.all
+  end
+
+  def show
+    @guest = Guest.find(params[:id])
+  end
+
+  def new
+    @guest = Guest.new
+  end
+
+  def edit
+    @guest = Guest.find(params[:id])
+  end
+
+  def create
+    @guest = Guest.new(guest_params)
+
+    if @guest.save
+      redirect_to @guest
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @guest = Guest.find(params[:id])
+
+    if @guest.update(guest_params)
+      redirect_to @guest
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @guest = Guest.find(params[:id])
+    @guest.destroy
+
+    redirect_to guests_path
+  end
+
+private
+  def guest_params
+    params.require(:guest).permit(:name, :email, :attending)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :guests
+
+  root 'guests#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
